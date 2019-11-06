3cdda82c381d101a2f2f50dfec2122e1a4b3b405
Matt Dole <mdole7@gmail.com>
2019-11-06 09:03:29 -0500

    starting in on navigation

--- App.tsx
+++ App.tsx
@@ -1,25 +1,52 @@
 import React from "react";
 import { StyleSheet, View, Button } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
-  const loadCombat = () => {
-    return null;
+class ProfileScreen extends React.Component {
+  static navigationOptions = {
+    title: "Profile"
   };
-  const loadSettings = () => {
-    return null;
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <Button title="Start Combat" onPress={loadCombat()} />
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Button title="Home" onPress={() => navigate("Home")} />
       </View>
-      <View style={styles.buttonContainer}>
-        <Button title="Character settings" onPress={loadSettings()} />
-      </View>
-    </View>
-  );
+    );
+  }
 }
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Welcome"
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Start Combat"
+            color="#434343"
+            onPress={() => {
+              alert("Starting combat!");
+            }}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Character settings"
+            onPress={() =>
+              this.props.navigation.navigate("Profile", { name: "Xavier" })
+            }
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -34,3 +61,19 @@ const styles = StyleSheet.create({
     flex: 1
   }
 });
+
+const MainNavigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Profile: { screen: ProfileScreen }
+  },
+  { initialRouteName: "Home" }
+);
+
+const AppContainer = createAppContainer(MainNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
