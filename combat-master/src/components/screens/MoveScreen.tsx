25d2aaddb0dcacb4a2b77a7caeec625bdabcd1ba
Matt Dole <mdole7@gmail.com>
2019-11-13 08:42:50 -0500

    super rudimentary movement grid

--- combat-master/src/components/screens/MoveScreen.tsx
+++ combat-master/src/components/screens/MoveScreen.tsx
@@ -1,22 +1,105 @@
 import React from "react";
 import { View, Button } from "react-native";
-import { NavigationInjectedProps } from "react-navigation";
+import SelectableGrid from "react-native-selectable-grid";
+import { NavigationInjectedProps, ScrollView } from "react-navigation";
 
 interface MoveScreenProps extends NavigationInjectedProps {}
 
+const gridData = [
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "0" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" },
+  { label: "" }
+];
+
 export class MoveScreen extends React.Component<MoveScreenProps> {
   static navigationOptions = {
-    title: "Action"
+    title: "Move"
   };
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Button
-          title="Take action"
-          onPress={() => alert("Taking an action!")}
-        />
-      </View>
+      <ScrollView>
+        <SelectableGrid data={gridData} maxPerRow={9} maxSelect={10} />
+        <Button title="Finished" onPress={() => navigate("MainCombatAction")} />
+      </ScrollView>
     );
   }
 }
