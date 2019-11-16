f6828620f8e3e671dda9a42d17e89da09ecf5188
Matt Dole <mdole7@gmail.com>
2019-11-16 13:43:27 -0500

    working towards storing character

--- combat-master/src/components/screens/ProfileScreen.tsx
+++ combat-master/src/components/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Button, TextInput } from "react-native";
+import { View, Button, TextInput, AsyncStorage } from "react-native";
 import { NavigationInjectedProps } from "react-navigation";
 import { Formik } from "formik";
 
@@ -11,22 +11,40 @@ interface characterValues {
   level: number;
 }
 
-const initialValues: characterValues = {
-  name: "Xavier Xiloscient",
-  class: "Bard",
-  level: 1
+const getCharacter = async () => {
+  let character;
+  try {
+    character = await AsyncStorage.getItem("@combatMaster_character");
+  } catch (e) {
+    alert(`No luck on that one, here's the error: ${e}`);
+  }
+  console.log(`Got your character, boss. ${character}`);
+  return character;
 };
+
+const retreivedCharacter = getCharacter();
+const initialValues = JSON.parse(retreivedCharacter);
 export class ProfileScreen extends React.Component<ProfileScreenProps> {
   static navigationOptions = {
     title: "Profile"
   };
   render() {
     const { navigate } = this.props.navigation;
+    const storeCharacter = async values => {
+      try {
+        await AsyncStorage.setItem(
+          "@combatMaster_character",
+          JSON.stringify(values)
+        );
+      } catch (e) {
+        alert(
+          `oh shoot had some trouble saving that one...here's the error if you're curious ${e}`
+        );
+      }
+      navigate("Home");
+    };
     return (
-      <Formik
-        initialValues={initialValues}
-        onSubmit={values => alert(JSON.stringify(values))}
-      >
+      <Formik initialValues={initialValues} onSubmit={storeCharacter}>
         {({ handleChange, handleBlur, handleSubmit, values }) => (
           <View>
             <TextInput
