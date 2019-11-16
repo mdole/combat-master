cf1e46e35e08756deebba5451512298ebcc3d669
Matt Dole <mdole7@gmail.com>
2019-11-16 13:04:26 -0500

    start using formik on profile screen

--- combat-master/src/components/screens/ProfileScreen.tsx
+++ combat-master/src/components/screens/ProfileScreen.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { View, Button, TextInput } from "react-native";
 import { NavigationInjectedProps } from "react-navigation";
+import { Formik } from "formik";
 
 interface ProfileScreenProps extends NavigationInjectedProps {}
 
+interface characterValues {
+  name: string;
+  class: string;
+  level: number;
+}
+
+const initialValues: characterValues = {
+  name: "Xavier Xiloscient",
+  class: "Bard",
+  level: 1
+};
 export class ProfileScreen extends React.Component<ProfileScreenProps> {
   static navigationOptions = {
     title: "Profile"
@@ -11,9 +23,34 @@ export class ProfileScreen extends React.Component<ProfileScreenProps> {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Button title="Home" onPress={() => navigate("Home")} />
-      </View>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={values => alert(JSON.stringify(values))}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <TextInput
+              placeholder="Character name"
+              onChangeText={handleChange("name")}
+              onBlur={handleBlur("name")}
+              values={values.name}
+            />
+            <TextInput
+              placeholder="Character class"
+              onChangeText={handleChange("class")}
+              onBlur={handleBlur("class")}
+              values={values.class}
+            />
+            <TextInput
+              placeholder="Character level"
+              onChangeTest={handleChange("level")}
+              onBlur={handleBlur("level")}
+              values={values.level}
+            />
+            <Button title="Submit" onPress={handleSubmit} />
+          </View>
+        )}
+      </Formik>
     );
   }
 }
