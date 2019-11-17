8bcd2e2079af39a7d83dbc4359ffc66bc9046d09
Matt Dole <mdole7@gmail.com>
2019-11-17 09:15:29 -0500

    makes actionscreen functional component

--- combat-master/src/components/screens/ActionScreen.tsx
+++ combat-master/src/components/screens/ActionScreen.tsx
@@ -1,28 +1,30 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, Button } from "react-native";
 import { NavigationInjectedProps } from "react-navigation";
+import { NavigationStackOptions } from "react-navigation-stack";
 
 interface ActionScreenProps extends NavigationInjectedProps {}
 
-export class ActionScreen extends React.Component<ActionScreenProps> {
-  static navigationOptions = {
-    title: "Action"
-  };
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <ScrollView>
-        <Text>Attack</Text>
-        <Text>Cast spell</Text>
-        <Text>Dash</Text>
-        <Text>Disengage</Text>
-        <Text>Dodge</Text>
-        <Text>Help</Text>
-        <Text>Hide</Text>
-        <Text>Ready</Text>
-        <Text>Search</Text>
-        <Text>Use object</Text>
-      </ScrollView>
-    );
-  }
-}
+export const ActionScreen: React.FC<ActionScreenProps> = props => {
+  const { navigate } = props.navigation;
+  return (
+    <ScrollView>
+      <Text>Attack</Text>
+      <Text>Cast spell</Text>
+      <Text>Dash</Text>
+      <Text>Disengage</Text>
+      <Text>Dodge</Text>
+      <Text>Help</Text>
+      <Text>Hide</Text>
+      <Text>Ready</Text>
+      <Text>Search</Text>
+      <Text>Use object</Text>
+      <Button
+        title="Back to main screen"
+        onPress={() => navigate("MainCombatAction")}
+      />
+    </ScrollView>
+  );
+};
+
+ActionScreen.navigationOptions = { title: "Action" };
