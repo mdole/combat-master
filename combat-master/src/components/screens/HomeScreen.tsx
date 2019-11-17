279c56b066b4be6f2a4051e7ce0e268c17efe29d
Matt Dole <mdole7@gmail.com>
2019-11-17 09:40:38 -0500

    makes homescreen functional component

--- combat-master/src/components/screens/HomeScreen.tsx
+++ combat-master/src/components/screens/HomeScreen.tsx
@@ -4,35 +4,30 @@ import { NavigationInjectedProps } from "react-navigation";
 
 interface HomeScreenProps extends NavigationInjectedProps {}
 
-export class HomeScreen extends React.Component<HomeScreenProps> {
-  static navigationOptions = {
-    title: "Welcome"
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Start Combat"
-            color="#434343"
-            onPress={() => {
-              this.props.navigation.navigate("MainCombatAction");
-            }}
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Character settings"
-            onPress={() =>
-              this.props.navigation.navigate("Profile", { name: "Xavier" })
-            }
-          />
-        </View>
+export const HomeScreen: React.FC<HomeScreenProps> = props => {
+  const { navigate } = props.navigation;
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Start Combat"
+          color="#434343"
+          onPress={() => {
+            navigate("MainCombatAction");
+          }}
+        />
       </View>
-    );
-  }
-}
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Character settings"
+          onPress={() => navigate("Profile", { name: "Xavier" })}
+        />
+      </View>
+    </View>
+  );
+};
+
+HomeScreen.navigationOptions = { title: "Welcome" };
 
 const styles = StyleSheet.create({
   container: {
