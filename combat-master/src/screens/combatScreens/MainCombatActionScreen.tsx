22af98875ab97bc47743cec457909fd083fffbf6
Matt Dole <mdole7@gmail.com>
2020-04-19 15:07:46 -0400

    pass updating movement through props from mainCombatScreen to movementScreen
    Co-authored-by: Barbara Muesing <muesingb@gmail.com>

--- combat-master/src/screens/combatScreens/MainCombatActionScreen.tsx
+++ combat-master/src/screens/combatScreens/MainCombatActionScreen.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { MainActionButton } from "../../components/MainActionButton";
-import { View } from "react-native";
+import { View, Text, Button } from "react-native";
 import { NavigationInjectedProps } from "react-navigation";
 
 interface MainCombatActionScreenProps extends NavigationInjectedProps {}
 
 export const MainCombatActionScreen: React.FC<MainCombatActionScreenProps> = (props) => {
   const { navigation } = props;
+  const [selectedMovement, updateSelectedMovement] = useState("");
+  const [selectedAction, updateSelectedAction] = useState("");
+
+  const updateMovement = () => {
+    console.log("hiiiii");
+    // updateSelectedMovement
+  };
+
   return (
     <View>
-      <MainActionButton buttonText="Move" destination="Move" navigation={navigation} />
+      <MainActionButton
+        buttonText="Move"
+        destination="Move"
+        navigation={navigation}
+        propsForPassing={{ selectedValue: selectedMovement, updateValue: updateMovement }}
+      />
+      <Text>{selectedMovement}</Text>
       <MainActionButton buttonText="Action" destination="Action" navigation={navigation} />
+      <Text>{selectedAction}</Text>
       <MainActionButton buttonText="Bonus Action" destination="BonusAction" navigation={navigation} />
     </View>
   );
--- combat-master/src/screens/combatScreens/MoveScreen.tsx
+++ combat-master/src/screens/combatScreens/MoveScreen.tsx
@@ -3,19 +3,27 @@ import { View, Button } from "react-native";
 import { NavigationInjectedProps } from "react-navigation";
 import { Toggle } from "../../components/Toggle";
 import { movement } from "../../components/movement";
-import { MoveCounter } from '../../components/MoveCounter'
+import { MoveCounter } from "../../components/MoveCounter";
 
 interface MoveScreenProps extends NavigationInjectedProps {}
 
 export const MoveScreen: React.FC<MoveScreenProps> = (props) => {
   const { navigate } = props.navigation;
+  const { params } = props.navigation.state;
+
   return (
     <View>
       {movement.map((move, index) => {
         return <Toggle label={move.label} bodyText={move.bodyText} key={index} />;
       })}
       <MoveCounter />
-      <Button title="Finished" onPress={() => navigate("MainCombatAction")} />
+      <Button
+        title="Finished"
+        onPress={() => {
+          params.updateValue();
+          navigate("MainCombatAction");
+        }}
+      />
     </View>
   );
 };
