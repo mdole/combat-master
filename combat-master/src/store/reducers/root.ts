aab4a2aad17ad0e20ec84827b4c28237521ee977
Matt Dole <mdole7@gmail.com>
2020-05-03 13:08:55 -0400

    adds redux and basic reducer
    Co-authored-by: Barbara Muesing <muesingb@gmail.com>

---
+++ combat-master/src/store/reducers/root.ts
@@ -0,0 +1,14 @@
+const initialState = {
+  selectedMovement: 0,
+  selectedAction: "",
+  selectedBonusAction: "",
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
