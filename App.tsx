1a94c6357e8da7acc3470faed0efb9da37deeb23
Matt Dole <mdole7@gmail.com>
2020-05-03 14:04:49 -0400

    configure store and provide to app
    Co-authored-by: Barbara Muesing <muesingb@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { Screens, initialRouteName } from "./combat-master/src/navigation";
+import { store } from "./combat-master/src/store/configureStore";
 
 const MainNavigator = createStackNavigator(Screens, {
-  initialRouteName: initialRouteName
+  initialRouteName: initialRouteName,
 });
 
 const AppContainer = createAppContainer(MainNavigator);
 
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
   }
 }
