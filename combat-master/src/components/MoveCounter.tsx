3ee16781d6e81dfa4cc1ee3678b9a0f4949dc1eb
Matt Dole <mdole7@gmail.com>
2020-05-31 14:59:25 -0400

    style the movement log
    Co-authored-by: Barbara Muesing <muesingb@gmail.com>

--- combat-master/src/components/MoveCounter.tsx
+++ combat-master/src/components/MoveCounter.tsx
@@ -2,17 +2,44 @@ import React, { useEffect } from "react";
 import { View, Text, Button } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { updateSelectedMoves, clearMoves, undoLastMove } from "../store/actions";
+import styled from "styled-components/native";
+import { CinzelRegular, LatoLight } from "./styledComponents/FontComponents";
 
 interface MoveCounterProps {
   movementInFeet: number;
   updateMovementInFeet: any;
 }
 
+const MovementButton = styled.TouchableOpacity`
+  background: rgba(86, 204, 242, 0.25);
+  border: 1px solid #000000;
+  border-radius: 2px;
+  width: 170px;
+  height: 50px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+const MovementContainer = styled.View`
+  background: rgba(245, 237, 214, 0.5);
+  align-items: center;
+  margin: 20px;
+  padding: 20px;
+  height: 75%;
+  overflow: hidden;
+`;
+
 export const MoveCounter: React.FC<MoveCounterProps> = (props) => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const { movementInFeet, updateMovementInFeet } = props;
+
   useEffect(() => {
     let newMovement = 0;
     let alternativeDiagonal = false;
@@ -33,24 +60,39 @@ export const MoveCounter: React.FC<MoveCounterProps> = (props) => {
 
   return (
     <View>
-      <Button title="Diagonal" onPress={() => dispatch(updateSelectedMoves("diagonal"))} />
-      <Button title="Orthogonal" onPress={() => dispatch(updateSelectedMoves("orthogonal"))} />
-      <Button
-        title="Undo last move"
-        onPress={() => {
-          dispatch(undoLastMove());
-        }}
-      />
-      <Button
-        title="Clear moves"
-        onPress={() => {
-          dispatch(clearMoves());
-        }}
-      />
-      {state.selectedMoves.map((move, index) => (
-        <Text key={index}>{move}</Text>
-      ))}
-      <Text>Total movement (feet): {movementInFeet}</Text>
+      <ButtonContainer>
+        <MovementButton onPress={() => dispatch(updateSelectedMoves("diagonal"))}>
+          <CinzelRegular size="20">Diagonal</CinzelRegular>
+        </MovementButton>
+        <MovementButton onPress={() => dispatch(updateSelectedMoves("orthogonal"))}>
+          <CinzelRegular size="20">Orthogonal</CinzelRegular>
+        </MovementButton>
+      </ButtonContainer>
+      <MovementContainer>
+        <LatoLight size="30">Total movement (feet):</LatoLight>
+        <LatoLight size="30">{movementInFeet}</LatoLight>
+        {state.selectedMoves.map((move: string, index) => (
+          <LatoLight size="20" key={index}>
+            {move}
+          </LatoLight>
+        ))}
+      </MovementContainer>
+      <ButtonContainer>
+        <MovementButton
+          onPress={() => {
+            dispatch(undoLastMove());
+          }}
+        >
+          <CinzelRegular size="20">Undo last move</CinzelRegular>
+        </MovementButton>
+        <MovementButton
+          onPress={() => {
+            dispatch(clearMoves());
+          }}
+        >
+          <CinzelRegular size="20">Clear moves</CinzelRegular>
+        </MovementButton>
+      </ButtonContainer>
     </View>
   );
 };
