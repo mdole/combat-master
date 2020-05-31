39822102d8308cc9d06435fc5f1151706db87f0b
Matt Dole <mdole7@gmail.com>
2020-05-31 14:35:46 -0400

    create reusable font components
    Co-authored-by: Barbara Muesing <muesingb@gmail.com>

---
+++ combat-master/src/components/styledComponents/FontComponents.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+
+export const CinzelRegular = styled.Text`
+  font-family: Cinzel_400Regular;
+  font-size: ${(props) => props.size};
+`;
+
+export const CinzelBold = styled.Text`
+  font-family: Cinzel_700Bold;
+  font-size: ${(props) => props.size};
+`;
+
+export const LatoLight = styled.Text`
+  font-family: Lato_300Light;
+  font-size: ${(props) => props.size};
+`;
