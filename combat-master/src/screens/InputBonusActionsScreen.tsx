7d9166696e772cac96e27b61e0c9e8dd5fcca73d
Matt Dole <mdole7@gmail.com>
2020-08-30 15:06:20 -0400

    style bonus action input
    Co-authored-by: Barbara Muesing <muesingb@gmail.com>

--- combat-master/src/screens/InputBonusActionsScreen.tsx
+++ combat-master/src/screens/InputBonusActionsScreen.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { StyledInput } from "../components/styledComponents/StyledInput";
 import styled from "styled-components/native";
-import { lightBlue, palePurple } from "../styles/colors";
+import { lightBlue, darkRed } from "../styles/colors";
 import { useSelector, useDispatch } from "react-redux";
-import { CinzelRegular } from "../components/styledComponents/FontComponents";
+import { CinzelRegular, CinzelBold, LatoLight } from "../components/styledComponents/FontComponents";
 import { FinishedButton } from "../components/FinishedButton";
 import { updateCharacter } from "../store/actions/characterActions";
 import { CharacterValues, storeCharacter } from "./ProfileScreen";
 import { NavigationInjectedProps } from "react-navigation";
+import { ParchmentBackground } from "../components/styledComponents/ParchmentBackground";
 
 export interface BonusAction {
   title: string;
@@ -25,14 +26,12 @@ const AddButton = styled.TouchableOpacity`
   justify-content: center;
 `;
 
-const RemoveActionButton = styled.TouchableOpacity`
-  background: ${palePurple};
-  border: 1px solid #000000;
-  border-radius: 2px;
-  width: 170px;
-  height: 50px;
-  align-items: center;
-  justify-content: center;
+const BonusActionList = styled.ScrollView`
+  border: solid 1px #000;
+  margin-bottom: 20;
+  width: 100%;
+  padding: 10px;
+  background-color: white;
 `;
 
 export const InputBonusActionsScreen: React.FC<NavigationInjectedProps> = (props) => {
@@ -45,57 +44,79 @@ export const InputBonusActionsScreen: React.FC<NavigationInjectedProps> = (props
   );
 
   return (
-    <View>
-      {characterToStore.bonusActions.map((action, index) => {
-        return (
-          <View key={index}>
-            <Text>Action: {action.title}</Text>
-            <Text>Description: {action.description}</Text>
-            <RemoveActionButton
-              onPress={() => {
-                const tempBonusActions = [...characterToStore.bonusActions];
-                tempBonusActions.splice(index, 1);
-                setCharacterToStore({
-                  ...characterToStore,
-                  bonusActions: [...tempBonusActions],
-                });
-              }}
-            >
-              <CinzelRegular size="20">Remove action</CinzelRegular>
-            </RemoveActionButton>
-          </View>
-        );
-      })}
-      <StyledInput
-        placeholder={"Enter a bonus action"}
-        onChangeText={(text) => setActionInput(text)}
-        value={actionInput}
-      />
-      <StyledInput
-        placeholder={"Enter a description"}
-        onChangeText={(text) => setDescriptionInput(text)}
-        value={descriptionInput}
-      />
-      <AddButton
-        onPress={() => {
-          setCharacterToStore({
-            ...characterToStore,
-            bonusActions: [...characterToStore.bonusActions, { title: actionInput, description: descriptionInput }],
-          });
-          setActionInput("");
-          setDescriptionInput("");
-        }}
-      >
-        <CinzelRegular size="20">Add Action</CinzelRegular>
-      </AddButton>
-      <FinishedButton
-        text="Save"
-        onPress={() => {
-          storeCharacter(characterToStore);
-          dispatch(updateCharacter(characterToStore));
-          props.navigation.goBack();
-        }}
-      />
-    </View>
+    <ParchmentBackground>
+      <View style={{ width: "100%", height: "100%" }}>
+        <View style={{ margin: 10, height: "100%" }}>
+          <BonusActionList>
+            {characterToStore.bonusActions.map((action, index) => {
+              return (
+                <View key={index} style={{ marginBottom: 15 }}>
+                  <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                    <CinzelBold size="24">{action.title}</CinzelBold>
+                    <CinzelBold
+                      size="24"
+                      color={darkRed}
+                      onPress={() => {
+                        const tempBonusActions = [...characterToStore.bonusActions];
+                        tempBonusActions.splice(index, 1);
+                        setCharacterToStore({
+                          ...characterToStore,
+                          bonusActions: [...tempBonusActions],
+                        });
+                      }}
+                    >
+                      X
+                    </CinzelBold>
+                  </View>
+                  <LatoLight size="14" numberOfLines={2}>
+                    {action.description}
+                  </LatoLight>
+                </View>
+              );
+            })}
+          </BonusActionList>
+
+          <LatoLight size="14" style={{ marginBottom: 10, alignSelf: "flex-start" }}>
+            Enter a bonus action
+          </LatoLight>
+          <StyledInput
+            placeholder={"Enter a bonus action"}
+            onChangeText={(text) => setActionInput(text)}
+            value={actionInput}
+            style={{ width: "100%" }}
+          />
+          <LatoLight size="14" style={{ marginBottom: 10, alignSelf: "flex-start" }}>
+            Describe your bonus action
+          </LatoLight>
+          <StyledInput
+            placeholder={"Enter a description"}
+            onChangeText={(text) => setDescriptionInput(text)}
+            value={descriptionInput}
+            style={{ width: "100%" }}
+          />
+          <AddButton
+            onPress={() => {
+              setCharacterToStore({
+                ...characterToStore,
+                bonusActions: [...characterToStore.bonusActions, { title: actionInput, description: descriptionInput }],
+              });
+              setActionInput("");
+              setDescriptionInput("");
+            }}
+            style={{ alignSelf: "center", marginBottom: 20 }}
+          >
+            <CinzelRegular size="20">Add</CinzelRegular>
+          </AddButton>
+          <FinishedButton
+            text="Save & return"
+            onPress={() => {
+              storeCharacter(characterToStore);
+              dispatch(updateCharacter(characterToStore));
+              props.navigation.goBack();
+            }}
+          />
+        </View>
+      </View>
+    </ParchmentBackground>
   );
 };
